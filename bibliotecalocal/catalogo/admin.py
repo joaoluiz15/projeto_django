0bfcc97820bf070b14fc4cadfd79940fb759363b
jackson1002 <jackson.freitas@escolar.ifrn.edu.br>
2023-10-06 17:25:59 -0300

    parte4

--- bibliotecalocal/catalogo/admin.py
+++ bibliotecalocal/catalogo/admin.py
@@ -2,9 +2,40 @@ from django.contrib import admin
 
 # Register your models here.
 
-from catalog.models import Author, Genre, Book, BookInstance
+from catalogo.models import Author, Genre, Book, BookInstance
 
-admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Book)
+#admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
+
+#classe de administrador definida
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+	fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+#registrando admin com o modelo associado
+admin.site.register(Author, AuthorAdmin)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+#classe BookAdmin definida
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'display_genre')
+	inlines = [BooksInstanceInline]
+	
+#classe BookInstanceAdmin definida
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+	list_filter = ('status', 'due_back')
+	
+	fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )	
