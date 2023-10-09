aa0c6e29a943441e1dd8d06c0955cd29818e7c71
joaoluiz15 <123167210+joaoluiz15@users.noreply.github.com>
2023-10-09 11:59:16 -0300

    parte 5
    fizemos a parte 5 até antes de "Página de detalhes do livro"

--- bibliotecalocal/catalogo/urls.py
+++ bibliotecalocal/catalogo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from catalogo import views
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
 ]
+
